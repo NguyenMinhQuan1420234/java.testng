2628dbb1d6ec10a9a053e9b6fd9497dabd134d61
Minh Quan <banchi0072000@gmail.com>
2022-07-06 14:25:15 +0700

    up

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,8 +1,19 @@
 package com.example;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test for simple App.
@@ -13,6 +24,20 @@ class AppTest {
      */
     @Test
     void testApp() {
-        assertEquals(1, 1);
+        System.setProperty("webdriver.chrome.driver", "/Users/bibuibanchi/Documents/chromedriver");
+        
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getTitle());
+
+        driver.findElement(By.id("inputUsername")).sendKeys("quannguyen@gmail.com");
+        driver.findElement(By.xpath("//input[@name='inputPassword']")).sendKeys("12345");
+        driver.findElement(By.xpath("//button[contains(text(),'Sign')]")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        WebElement error = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//p[@class='error']"))));
+        String ActualText = error.getText();
+        assertEquals("* Incorrect username or password", ActualText);
     }
 }
